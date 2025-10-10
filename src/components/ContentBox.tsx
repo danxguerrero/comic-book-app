6c16199018cd4c9e46d3231f92311c11794cea3a
danxguerrero <danxguerrero@yahoo.com>
2025-10-10 15:12:25 -0400

    Enhance ContentBox layout with responsivity for different screen sizes

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -4,9 +4,12 @@ export const ContentBox = () => {
     return (
         <div className="flex min-h-[40vh] border">
         <div className="min-h-full w-full">
-          <div className="flex h-full items-center mx-12">
-            <div className="w-1/2">Upload your comics</div>
-            <div className="w-1/2 h-full relative">
+          <div className="flex flex-col md:flex-row h-full items-center mx-12 gap-4 md:gap-0">
+            <div className="w-full md:w-1/2">
+                <h2>Upload your comics</h2>
+                <p>Share your creations with the comic community</p>
+            </div>
+            <div className="w-full md:w-1/2 h-full relative">
               <Image 
                 src="/CU_Hero_image.png" 
                 alt="ComicUnity" 
